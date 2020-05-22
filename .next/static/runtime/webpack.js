bc2613fac67c9b08e4bb87bba2af2af3ed4336d5
felixatchur <liuzhengyangg@gmail.com>
2020-05-22 13:59:17 +1000

    update button hover animatio

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f3d438c74cf428e591c1";
+/******/ 	var hotCurrentHash = "044aba1fa3867e989c94";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
